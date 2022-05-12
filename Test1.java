957406102fc33704c63e227dc059cdf838976462
sunkesulaprasanna <104551959+sunkesulaprasanna@users.noreply.github.com>
2022-05-12 12:33:20 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,18 @@
+public class Test1
+{
+public static void main(String[] args)
+{
+double a=30;
+double b=Math.toRadians(a);
+System.out.println("sine value of a is:" +Math.sin(a));
+System.out.println("cosine value of a is:" +Math.cos(a));
+System.out.println("tangent value of a is:" +Math.tan(a));
+}
+}
+
+
+output:
+sine value of a is:-0.9880316240928618
+cosine value of a is:0.15425144988758405
+tangent value of a is:-6.405331196646276
+
