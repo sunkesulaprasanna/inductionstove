60e43d4c76427866a0b140c4d3a2fbc7143720b6
sunkesulaprasanna <104551959+sunkesulaprasanna@users.noreply.github.com>
2022-05-12 12:32:57 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test
+{
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c=25;
+int d=25;
+System.out.println("a+b=" +(a+b));
+System.out.println("a-b=" +(a-b));
+System.out.println("a*b=" +(a*b));
+System.out.println("b/a=" +(b/a));
+}
+}
+
+output:
+a+b=30
+a-b=-10
+a*b=200
+b/a=2
+
+
